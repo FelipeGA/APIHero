ce1c5bf282b80ab1ecc3b46f112fca6fc4a68ec8
FelipeGA <lipegusmao@yahoo.com.br>
2021-02-15 21:54:18 -0300

    Criação da interface de repositório.

---
+++ src/main/java/com/ProjetoInterJavaDeveloper/APIHero/repository/HeroesRepository.java
@@ -0,0 +1,10 @@
+package com.ProjetoInterJavaDeveloper.APIHero.repository;
+
+import com.ProjetoInterJavaDeveloper.APIHero.modelo.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends  CrudRepository<Heroes,String> {
+    
+}
